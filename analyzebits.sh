26a3f4509a10a88233633aa2f1aab0ee6ee59f6a
Santosh Dwivedi <santosh@sanscreative.org>
2014-04-18 09:27:14 +0200

    Update analyzebits.sh

--- analyzebits.sh
+++ analyzebits.sh
@@ -1,3 +1,12 @@
+#Copyright 2014@Santosh Dwivedi, All Rights Reserved.
+#The code contained herein is licensed under the GNU General Public
+#License. You may obtain a copy of the GNU General Public License
+#Version 2 or later at the following locations:
+# http://www.opensource.org/licenses/gpl-license.html
+# http://www.gnu.org/copyleft/gpl.html
+
+#!/bin/bash
+# This script is to define and decode cpu register bits.
 Configure() {
 OUTPUT=$(zenity --forms --title="Configure register" --text="cpu register" --add-entry="name") 
 accepted=$?
